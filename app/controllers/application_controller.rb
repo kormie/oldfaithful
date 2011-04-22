0aad02a4ba3931450c599940ce5f47329d50564f
David Kormushoff <kormie@gmail.com>
2011-04-22 10:06:43 -0500

    Application Controller Refactoring

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,13 +14,13 @@ class ApplicationController < ActionController::Base
     def after_sign_out_path_for(resource_or_scope)
       intro_index_path
     end
+    
     def after_update_path_for(resource_or_scope)
       users_index_path
     end
     def find_current_user
-      current_user
-      @user = User.find(current_user.id)
-      @courses = @user.courses.all
+      @user = current_user
+      @courses = @user.courses
     end
     
     def nav_spots
@@ -33,8 +33,10 @@ class ApplicationController < ActionController::Base
       return @nav_spots
     end
     
+
     @welcome_message ||= "Welcome to Jot Locker"
-    @day_number = DateTime.now.strftime('%d')
-    @long_day = DateTime.now.strftime('%A, %m.%d')
     @today = DateTime.now
+    @day_number = @today.strftime('%d')
+    @long_day = @today.strftime('%A, %m.%d')
+    
 end
